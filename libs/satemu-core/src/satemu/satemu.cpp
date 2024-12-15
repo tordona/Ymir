7ffbd2b4dd7ec4713769c181465f950ca7b79da0
StrikerX3 <StrikerX3@users.noreply.github.com>
2024-12-15 14:03:35 -0300

    Main: Require CHAR_BIT == 8

--- libs/satemu-core/src/satemu/satemu.cpp
+++ libs/satemu-core/src/satemu/satemu.cpp
@@ -4,6 +4,7 @@
 #include <bit>
 
 static_assert(std::endian::native == std::endian::little, "big-endian platforms are not supported at this moment");
+static_assert(CHAR_BIT == 8, "char is expected to have 8 bits");
 
 namespace satemu {
 
