29ad808fe57dfb4be395399e3a4dfa82ebf07698
StrikerX3 <StrikerX3@users.noreply.github.com>
2025-02-01 21:27:31 -0300

    VDP2: Honor per-dot coefficient increment mode properly

--- libs/satemu-core/src/satemu/hw/vdp/vdp.cpp
+++ libs/satemu-core/src/satemu/hw/vdp/vdp.cpp
@@ -1400,11 +1400,24 @@ void VDP::VDP2CalcRotationParameterTables() {
         const uint32 xShift = doubleResH ? 1 : 0;
         const uint32 maxX = m_HRes >> xShift;
 
+        // Use per-dot coefficient if reading from CRAM or if any of the VRAM banks was designated as coefficient data
+        bool perDotCoeff = m_VDP2.RAMCTL.CRKTE;
+        if (!m_VDP2.RAMCTL.CRKTE) {
+            perDotCoeff = m_VDP2.RAMCTL.RDBSA0n == 1 || m_VDP2.RAMCTL.RDBSB0n == 1;
+            if (m_VDP2.RAMCTL.VRAMD) {
+                perDotCoeff |= m_VDP2.RAMCTL.RDBSA1n == 1;
+            }
+            if (m_VDP2.RAMCTL.VRBMD) {
+                perDotCoeff |= m_VDP2.RAMCTL.RDBSB1n == 1;
+            }
+        }
+
+        Coefficient coeff = VDP2FetchRotationCoefficient(params, KA);
+
         // Precompute whole line
         for (uint32 x = 0; x < maxX; x++) {
             // Replace parameters with those obtained from the coefficient table if enabled
             if (params.coeffTableEnable) {
-                const Coefficient coeff = VDP2FetchRotationCoefficient(params, KA);
                 state.lineColorData[x] = coeff.lineColorData;
                 state.transparent[x] = coeff.transparent;
 
@@ -1416,8 +1429,11 @@ void VDP::VDP2CalcRotationParameterTables() {
                 case ViewpointX: Xp = coeff.value; break;
                 }
 
-                // Increment coefficient table address by Hcnt
-                KA += t.dKAx;
+                // Increment coefficient table address by Hcnt if using per-dot coefficients
+                if (perDotCoeff) {
+                    KA += t.dKAx;
+                    coeff = VDP2FetchRotationCoefficient(params, KA);
+                }
             }
 
             // Store screen coordinates
@@ -2223,51 +2239,6 @@ Coefficient VDP::VDP2FetchRotationCoefficient(const RotationParams &params, uint
 
     const uint32 baseAddress = params.coeffTableAddressOffset;
     const uint32 offset = coeffAddress >> 10u;
-
-    // Check that the VRAM bank containing the coefficient table is designated for coefficient data.
-    // Return a default (transparent) coefficient if not.
-    if (!m_VDP2.RAMCTL.CRKTE) {
-        // Determine which bank is targeted
-        const uint32 address = ((baseAddress + offset) * sizeof(uint32)) >> params.coeffDataSize;
-
-        // Address is 19 bits wide when using 512 KiB VRAM.
-        // Bank is designated by bits 17-18.
-        uint32 bank = bit::extract<17, 18>(address);
-
-        // RAMCTL.VRAMD and VRBMD specify if VRAM A and B respectively are partitioned into two blocks (when set).
-        // If they're not partitioned, RDBSA0n/RDBSB0n designate the role of the whole block (VRAM-A or -B).
-        // RDBSA1n/RDBSB1n designates the roles of the second half of the partitioned banks (VRAM-A1 or -A2).
-        // Masking the bank index with VRAMD/VRBMD adjusts the bank index of the second half back to the first half so
-        // we can uniformly handle both cases with one simple switch table.
-        if (bank < 2) {
-            bank &= ~(m_VDP2.RAMCTL.VRAMD ^ 1);
-        } else {
-            bank &= ~(m_VDP2.RAMCTL.VRBMD ^ 1);
-        }
-
-        switch (bank) {
-        case 0: // VRAM-A0 or VRAM-A
-            if (m_VDP2.RAMCTL.RDBSA0n != 1) {
-                return coeff;
-            }
-            break;
-        case 1: // VRAM-A1
-            if (m_VDP2.RAMCTL.RDBSA1n != 1) {
-                return coeff;
-            }
-            break;
-        case 2: // VRAM-B0 or VRAM-B
-            if (m_VDP2.RAMCTL.RDBSB0n != 1) {
-                return coeff;
-            }
-            break;
-        case 3: // VRAM-B1
-            if (m_VDP2.RAMCTL.RDBSB1n != 1) {
-                return coeff;
-            }
-            break;
-        }
-    }
 
     if (params.coeffDataSize == 1) {
         // One-word coefficient data
