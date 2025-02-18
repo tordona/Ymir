7c4a4938e312818ab93f6b0989528312e8ef44bf
StrikerX3 <StrikerX3@users.noreply.github.com>
2025-02-18 13:33:12 -0300

    chore: Reformat files

--- libs/satemu-core/src/satemu/hw/cdblock/cdblock.cpp
+++ libs/satemu-core/src/satemu/hw/cdblock/cdblock.cpp
@@ -67,8 +67,8 @@ void CDBlock::Reset(bool hard) {
     m_mpegAuthStatus = 0;
 
     if (hard) {
-    m_HIRQ = 0x0BC1; // 0x0BE1;
-    m_HIRQMASK = 0;
+        m_HIRQ = 0x0BC1; // 0x0BE1;
+        m_HIRQMASK = 0;
     } else {
         m_HIRQ &= kHIRQ_mask & ~kHIRQ_DCHG;
     }
