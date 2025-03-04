939f86d7fb1b4cb003292feef97dfa9d93655086
StrikerX3 <StrikerX3@users.noreply.github.com>
2025-03-04 09:55:14 -0300

    SCU: Stub DSP tests

---
+++ tests/satemu-core-tests/src/hw/scu/scu_dsp_tests.cpp
@@ -0,0 +1,85 @@
+#include <catch2/catch_test_macros.hpp>
+
+#include <satemu/hw/scu/scu_dsp.hpp>
+
+using namespace satemu;
+
+namespace scu_dsp {
+
+struct TestSubject {
+    mutable sys::Bus bus;
+    mutable scu::SCUDSP dsp{bus};
+
+    bool dspEndTriggered = false;
+
+    TestSubject() {
+        dsp.SetTriggerDSPEndCallback(util::MakeClassMemberRequiredCallback<&TestSubject::TriggerDSPEnd>(this));
+    }
+
+    void ClearAll() {
+        dspEndTriggered = false;
+    }
+
+    void TriggerDSPEnd() {
+        dspEndTriggered = true;
+    }
+};
+
+TEST_CASE_PERSISTENT_FIXTURE(TestSubject, "SCU DSP instructions execute correctly", "[scu][scudsp][instructions]") {
+    dsp.Reset(true);
+
+    SECTION("ALU - AND") {}
+
+    SECTION("ALU - OR") {}
+
+    SECTION("ALU - XOR") {}
+
+    SECTION("ALU - ADD") {}
+
+    SECTION("ALU - SUB") {}
+
+    SECTION("ALU - AD2") {}
+
+    SECTION("ALU - SR") {}
+
+    SECTION("ALU - RR") {}
+
+    SECTION("ALU - SL") {}
+
+    SECTION("ALU - RL") {}
+
+    SECTION("ALU - RL8") {}
+
+    SECTION("X-Bus - MOV MUL,P") {}
+
+    // [s] = CT0..CT3, MC0..MC3
+    SECTION("X-Bus - MOV [s],P") {}
+
+    // [s] = CT0..CT3, MC0..MC3
+    SECTION("X-Bus - MOV [s],X") {}
+
+    SECTION("Y-Bus - CLR A") {}
+
+    SECTION("Y-Bus - MOV ALU,A") {}
+
+    // [s] = CT0..CT3, MC0..MC3
+    SECTION("Y-Bus - MOV [s],A") {}
+
+    // [s] = CT0..CT3, MC0..MC3
+    SECTION("Y-Bus - MOV [s],Y") {}
+
+    // [d] =
+    SECTION("D1-Bus - MOV SImm,[d]") {}
+
+    // [s] = CT0..CT3, MC0..MC3, ALUL, ALUH
+    // [d] = CT0..CT3, MC0..MC3, RX, P, RA0, WA0, LOP, TOP
+    SECTION("D1-Bus - MOV [s],[d]") {}
+
+    SECTION("Combined - AND  MOV MUL,P  MOV[s],X  MOV ALU,A  MOV [s],Y  MOV [s],[d]") {}
+}
+
+// TODO: test complete programs
+
+// TODO: test DMA
+
+} // namespace scu_dsp
