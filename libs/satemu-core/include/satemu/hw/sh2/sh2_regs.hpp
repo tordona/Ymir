aaa756df8333f5341338b5e8991c5fa2f65ddf30
StrikerX3 <StrikerX3@users.noreply.github.com>
2025-03-06 20:03:48 -0300

    SH2: Properly name the SR flags

--- libs/satemu-core/include/satemu/hw/sh2/sh2_regs.hpp
+++ libs/satemu-core/include/satemu/hw/sh2/sh2_regs.hpp
@@ -16,12 +16,12 @@ union RegMAC {
 union RegSR {
     uint32 u32;
     struct {
-        uint32 T : 1;      //   0  True flag
-        uint32 S : 1;      //   1  Used by multiply/accumulate
+        uint32 T : 1;      //   0  Test flag
+        uint32 S : 1;      //   1  Saturate - Used by multiply/accumulate
         uint32 : 2;        // 2-3  (reserved, must be zero)
         uint32 ILevel : 4; // 4-7  Interrupt mask
-        uint32 Q : 1;      //   8  Used by DIV0U/S and DIV1
-        uint32 M : 1;      //   9  Used by DIV0U/S and DIV1
+        uint32 Q : 1;      //   8  Quotient - Used by DIV0U/S and DIV1
+        uint32 M : 1;      //   9  Modulus - Used by DIV0U/S and DIV1
                            // (remaining bits are reserved and must be zero)
     };
 };
