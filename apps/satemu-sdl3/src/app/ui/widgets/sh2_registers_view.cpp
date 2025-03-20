d1baf3220f777cd03724ff36217380bacbbab815
StrikerX3 <StrikerX3@users.noreply.github.com>
2025-03-20 19:19:36 -0300

    App: Slight improvements to SH2 registers layout

--- apps/satemu-sdl3/src/app/ui/widgets/sh2_registers_view.cpp
+++ apps/satemu-sdl3/src/app/ui/widgets/sh2_registers_view.cpp
@@ -17,6 +17,10 @@ void SH2RegistersView::Display() {
     if (m_master || m_context.saturn.slaveSH2Enabled) {
         // TODO: improve layout - two compact columns
 
+        ImGui::PushFont(m_context.fonts.monospaceMedium);
+        const float hexCharWidth = ImGui::CalcTextSize("F").x;
+        ImGui::PopFont();
+
         auto &regs = m_sh2.GetGPRs();
 
         auto drawReg32 = [&](std::string name, uint32 value) {
@@ -26,8 +30,7 @@ void SH2RegistersView::Display() {
             ImGui::SameLine(50.0f);
 
             ImGui::PushFont(m_context.fonts.monospaceMedium);
-            float charWidth = ImGui::CalcTextSize("F").x;
-            ImGui::SetNextItemWidth(ImGui::GetStyle().FramePadding.x * 2 + charWidth * 8);
+            ImGui::SetNextItemWidth(ImGui::GetStyle().FramePadding.x * 2 + hexCharWidth * 8);
             const std::string lblField = fmt::format("##input_{}", name);
             ImGui::InputScalar(lblField.c_str(), ImGuiDataType_U32, &value, nullptr, nullptr, "%08X",
                                ImGuiInputTextFlags_CharsHexadecimal);
@@ -45,44 +48,55 @@ void SH2RegistersView::Display() {
         drawReg32("MACH", mac.H);
         drawReg32("MACL", mac.L);
 
+        drawReg32("GBR", m_sh2.GetGBR());
+        drawReg32("VBR", m_sh2.GetVBR());
+
         auto sr = m_sh2.GetSR();
         drawReg32("SR", sr.u32);
         bool M = sr.M;
         bool Q = sr.Q;
         bool S = sr.S;
         bool T = sr.T;
+        uint8 ILevel = sr.ILevel;
 
+        ImGui::PushStyleVarX(ImGuiStyleVar_ItemSpacing, 4.0f);
         ImGui::BeginGroup();
         ImGui::Checkbox("##M", &M);
-        ImGui::Text("M");
+        ImGui::TextUnformatted("M");
         ImGui::EndGroup();
 
         ImGui::SameLine();
 
         ImGui::BeginGroup();
         ImGui::Checkbox("##Q", &Q);
-        ImGui::Text("Q");
+        ImGui::TextUnformatted("Q");
         ImGui::EndGroup();
 
         ImGui::SameLine();
 
         ImGui::BeginGroup();
         ImGui::Checkbox("##S", &S);
-        ImGui::Text("S");
+        ImGui::TextUnformatted("S");
         ImGui::EndGroup();
 
         ImGui::SameLine();
 
         ImGui::BeginGroup();
         ImGui::Checkbox("##T", &T);
-        ImGui::Text("T");
+        ImGui::TextUnformatted("T");
         ImGui::EndGroup();
 
-        // TODO: make this editable
-        ImGui::Text("I=%X", (uint8)sr.ILevel);
+        ImGui::SameLine();
 
-        drawReg32("GBR", m_sh2.GetGBR());
-        drawReg32("VBR", m_sh2.GetVBR());
+        ImGui::BeginGroup();
+        ImGui::PushFont(m_context.fonts.monospaceMedium);
+        ImGui::SetNextItemWidth(ImGui::GetStyle().FramePadding.x * 2 + hexCharWidth * 1);
+        ImGui::InputScalar("##input_SR_ILevel", ImGuiDataType_U8, &ILevel, nullptr, nullptr, "%X",
+                           ImGuiInputTextFlags_CharsHexadecimal);
+        ImGui::PopFont();
+        ImGui::TextUnformatted("I");
+        ImGui::EndGroup();
+        ImGui::PopStyleVar();
 
         /*if (debugTrace) {
             drawText(x, y + 280, "vec lv");
