562640c888253880b1e008774bd5b7307c504724
StrikerX3 <StrikerX3@users.noreply.github.com>
2025-03-21 21:18:43 -0300

    chore: Remove unused frontend base project

--- libs/satemu-frontend-base/include/satemu/frontend/base.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <satemu/export.h>
-
-namespace satemu::frontend {
-
-// LIB_EXPORT void func();
-
-} // namespace satemu::frontend
--- libs/satemu-frontend-base/src/satemu/frontend/base.cpp
+++
@@ -1,8 +0,0 @@
-#include <satemu/export.h>
-#include <satemu/frontend/base.hpp>
-
-namespace satemu::frontend {
-
-// LIB_EXPORT void func() {}
-
-} // namespace satemu::frontend
