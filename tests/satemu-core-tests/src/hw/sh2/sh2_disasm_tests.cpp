32f0d5c343d4e4c7a97291c9f4e3787de63284c8
StrikerX3 <StrikerX3@users.noreply.github.com>
2025-03-22 20:26:15 -0300

    Tests: Fix broken unit test after recent changes to SH2 disassembler

--- tests/satemu-core-tests/src/hw/sh2/sh2_disasm_tests.cpp
+++ tests/satemu-core-tests/src/hw/sh2/sh2_disasm_tests.cpp
@@ -6,13 +6,16 @@ using namespace satemu;
 
 TEST_CASE("SH2 disassembler smoke tests", "[sh2][disasm][smoke]") {
     SECTION("mov.l @(0x0600035C), r6") {
+        const uint32 pc = 0x0600035A;
         const auto &disasm = sh2::g_disasmTable.disasm[0xD600];
 
         REQUIRE(disasm.mnemonic == sh2::Mnemonic::MOV);
+        CHECK(disasm.validInDelaySlot == true);
         CHECK(disasm.opSize == sh2::OperandSize::Long);
-        CHECK(disasm.op1.type == sh2::Operand::Type::AtDispPC);
-        CHECK(disasm.op1.immDisp == 0);
+        CHECK(disasm.op1.type == sh2::Operand::Type::AtDispPCWordAlign);
+        CHECK(disasm.op1.immDisp == 4);
         CHECK(disasm.op2.type == sh2::Operand::Type::Rn);
         CHECK(disasm.op2.reg == 6);
+        CHECK((pc & ~3) + disasm.op1.immDisp == 0x0600035C);
     }
 }
