ffedeb830adde12363512d636f736461d48b549e
StrikerX3 <StrikerX3@users.noreply.github.com>
2025-04-17 22:12:43 -0300

    feat(sys): Simple IPL ROM database

---
+++ libs/satemu-core/include/satemu/db/ipl_db.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <satemu/core/hash.hpp>
+
+namespace satemu::db {
+
+enum class SystemVariant { None, SaturnJP, SaturnUS_EU, HiSaturn, GameNaviHiSaturn, SamsungSaturn, VSaturn, DevKit };
+
+struct IPLROMInfo {
+    const char *commonFilename;
+    const char *version;
+    uint16_t year;
+    uint8_t month;
+    uint8_t day;
+    SystemVariant sysVariant;
+};
+
+// Retrieves information about an IPL ROM image given its XXH128 hash.
+// Returns nullptr if there is no information for the given hash.
+const IPLROMInfo *GetInfo(Hash128 hash);
+
+} // namespace satemu::db
