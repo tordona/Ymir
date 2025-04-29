3a8d13ea7c967d6514fe15e1782994a34d7fc370
StrikerX3 <StrikerX3@users.noreply.github.com>
2025-04-28 22:19:57 -0300

    docs: Document version.hpp

--- libs/ymir-core/include/ymir/version.hpp
+++ libs/ymir-core/include/ymir/version.hpp
@@ -1,10 +1,15 @@
 #pragma once
 
+/**
+@file
+@brief Ymir library version definitions.
+*/
+
 namespace ymir::version {
 
-inline constexpr auto string = Ymir_VERSION;
-inline constexpr auto major = Ymir_VERSION_MAJOR;
-inline constexpr auto minor = Ymir_VERSION_MINOR;
-inline constexpr auto patch = Ymir_VERSION_PATCH;
+inline constexpr auto string = Ymir_VERSION; ///< The library version string in the format "<major>.<minor>.<patch>"
+inline constexpr auto major = Ymir_VERSION_MAJOR; ///< The major version of the library
+inline constexpr auto minor = Ymir_VERSION_MINOR; ///< The minor version of the library
+inline constexpr auto patch = Ymir_VERSION_PATCH; ///< The patch version of the library
 
 } // namespace ymir::version
