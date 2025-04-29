05ad666f8824678f46db1c94ebb1234f4e96d519
StrikerX3 <StrikerX3@users.noreply.github.com>
2025-04-28 22:19:47 -0300

    docs: Document unreachable.hpp

--- libs/ymir-core/include/ymir/util/unreachable.hpp
+++ libs/ymir-core/include/ymir/util/unreachable.hpp
@@ -1,7 +1,16 @@
 #pragma once
 
+/**
+@file
+@brief Defines the `util::unreachable()` function that marks code as unreachable.
+*/
+
 namespace util {
 
+/// @brief Marks a point in code as unreachable.
+///
+/// This is a compiler hint that should be used in places where it is never expected to be reached.
+/// It invokes undefined behavior, which may lead to crashes if execution *does* reach this function.
 [[noreturn]] inline void unreachable() {
 #if defined(__clang__) || defined(__GNUC__) || defined(__GNUG__)
     __builtin_unreachable();
