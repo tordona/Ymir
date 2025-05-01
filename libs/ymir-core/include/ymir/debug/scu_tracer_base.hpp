236ef7a5810ffbc1fb7ea3f71ca40a9d3a04199b
StrikerX3 <StrikerX3@users.noreply.github.com>
2025-05-01 13:53:23 -0300

    docs: Document both tracers

--- libs/ymir-core/include/ymir/debug/scu_tracer_base.hpp
+++ libs/ymir-core/include/ymir/debug/scu_tracer_base.hpp
@@ -1,58 +1,58 @@
 #pragma once
 
+/**
+@file
+@brief Defines `ymir::debug::ISCUTracer`, the SCU tracer interface.
+*/
+
 #include <ymir/core/types.hpp>
 
 namespace ymir::debug {
 
-// Interface for SCU tracers.
-//
-// Must be implemented by users of the core library.
-//
-// Attach to an instance of `ymir::scu::SCU` with its `UseTracer(ISCUTracer *)` method.
+/// @brief Interface for SCU tracers.
+///
+/// Must be implemented by users of the core library.
+///
+/// Attach to an instance of `ymir::scu::SCU` with its `UseTracer(ISCUTracer *)` method.
 struct ISCUTracer {
+    /// @brief Default virtual destructor. Required for inheritance.
     virtual ~ISCUTracer() = default;
 
-    // Invoked when the SCU raises an interrupt.
-    //
-    // `index` is the interrupt index. See documentation for `scu::InterruptStatus`.
-    // `level` is the interrupt level.
+    /// @brief Invoked when the SCU raises an interrupt.
+    /// @param[in] index the interrupt index; see `ymir::scu::InterruptStatus`
+    /// @param[in] level the interrupt level
     virtual void RaiseInterrupt(uint8 index, uint8 level) {}
 
-    // Invoked when the SCU acknowledges an interrupt.
-    //
-    // `index` is the interrupt index. See documentation for `scu::InterruptStatus`.
+    /// @brief Invoked when the SCU acknowledges an interrupt.
+    /// @param[in] index the interrupt index; see `ymir::scu::InterruptStatus`
     virtual void AcknowledgeInterrupt(uint8 index) {}
 
-    // Invoked when a byte is writted to mednafen's debug port.
-    //
-    // `ch` is the character written to the port.
+    /// @brief Invoked when a byte is writted to mednafen's debug port.
+    /// @param[in] ch the character written to the port
     virtual void DebugPortWrite(uint8 ch) {}
 
-    // Invoked when a DMA transfer begins.
-    // Also invoked on every indirect transfer entry.
-    //
-    // `channel` is the channel index.
-    // `srcAddr` is the starting source address.
-    // `dstAddr` is the starting destination address.
-    // `xferCount` is the number of bytes to be transferred.
-    // `srcAddrInc` is the source address increment per transfer.
-    // `dstAddrInc` is the destination address increment per transfer.
-    // `indirect` indicates if this is a direct (false) or indirect transfer (true).
-    // `indirectAddr` is the address of the indirect transfer data.
+    /// @brief Invoked when a DMA transfer begins and on every indirect transfer.
+    /// @param[in] channel the channel index
+    /// @param[in] srcAddr the starting source address
+    /// @param[in] dstAddr the starting destination address
+    /// @param[in] xferCount the number of bytes to be transferred
+    /// @param[in] srcAddrInc the source address increment per transfer
+    /// @param[in] dstAddrInc the destination address increment per transfer
+    /// @param[in] indirect indicates if this is a direct (`false`) or indirect transfer (`true`)
+    /// @param[in] indirectAddr the address of the indirect transfer data
     virtual void DMA(uint8 channel, uint32 srcAddr, uint32 dstAddr, uint32 xferCount, uint32 srcAddrInc,
                      uint32 dstAddrInc, bool indirect, uint32 indirectAddr) {}
 
     // -----------------------------------------------------------------------------------------------------------------
     // DSP
 
-    // Invoked when a DSP DMA transfer begins.
-    //
-    // `toD0` indicates the direction of the transfer: from DSP to D0 (true) or from D0 to DSP (false).
-    // `addrD0` is the address on the D0 bus.
-    // `addrDSP` is the addres on the DSP: 0-3 for Data RAM banks 0-3, 4 for Program RAM.
-    // `count` is the number of longword transfers to be performed.
-    // `addrInc` is the D0 address increment per transfer.
-    // `hold` indicates if the D0 address will be updated (false) or not (true) after the transfer.
+    /// @brief Invoked when a DSP DMA transfer begins.
+    /// @param[in] toD0 indicates the direction of the transfer: from DSP to D0 (`true`) or from D0 to DSP (`false`)
+    /// @param[in] addrD0 the address on the D0 bus
+    /// @param[in] addrDSP the addres on the DSP: 0-3 for Data RAM banks 0-3, 4 for Program RAM
+    /// @param[in] count the number of longword transfers to be performed
+    /// @param[in] addrInc the D0 address increment per transfer
+    /// @param[in] hold indicates if the D0 address will be updated (`false`) or not (`true`) after the transfer
     virtual void DSPDMA(bool toD0, uint32 addrD0, uint8 addrDSP, uint8 count, uint8 addrInc, bool hold) {}
 };
 
--- libs/ymir-core/include/ymir/debug/sh2_tracer_base.hpp
+++ libs/ymir-core/include/ymir/debug/sh2_tracer_base.hpp
@@ -1,91 +1,90 @@
 #pragma once
 
+/**
+@file
+@brief Defines `ymir::debug::ISH2Tracer`, the SH2 tracer interface.
+*/
+
 #include <ymir/core/types.hpp>
 
 #include <ymir/hw/sh2/sh2_intc.hpp>
 
 namespace ymir::debug {
 
-// Interface for SH2 tracers.
-//
-// Must be implemented by users of the core library.
-//
-// Attach to an instance of `ymir::sh2::SH2` with its `UseTracer(ISH2Tracer *)` method.
-//
-// This tracer requires the emulator to execute in debug mode.
+/// @brief Interface for SH2 tracers.
+///
+/// Must be implemented by users of the core library.
+///
+/// Attach to an instance of `ymir::sh2::SH2` with its `UseTracer(ISH2Tracer *)` method.
+///
+/// @note This tracer requires the emulator to execute in debug tracing mode.
 struct ISH2Tracer {
+    /// @brief Default virtual destructor. Required for inheritance.
     virtual ~ISH2Tracer() = default;
 
-    // Invoked immediately before executing an instruction.
-    //
-    // `pc` is the current program counter
-    // `opcode` is the instruction opcode
-    // `delaySlot` indicates if the instruction is executing in a delay slot
+    /// @brief Invoked immediately before executing an instruction.
+    /// @param[in] pc the current program counter
+    /// @param[in] opcode the instruction opcode
+    /// @param[in] delaySlot indicates if the instruction is executing in a delay slot
     virtual void ExecuteInstruction(uint32 pc, uint16 opcode, bool delaySlot) {}
 
-    // Invoked when the CPU handles an interrupt.
-    //
-    // `vecNum` is the interrupt vector number
-    // `level` is the interrupt level (priority)
-    // `source` is the interrupt source
-    // `pc` is the value of PC at the moment the interrupt was handled
+    /// @brief Invoked when the CPU handles an interrupt.
+    /// @param[in] vecNum the interrupt vector number
+    /// @param[in] level the interrupt level (priority)
+    /// @param[in] source the interrupt source
+    /// @param[in] pc the value of PC at the moment the interrupt was handled
     virtual void Interrupt(uint8 vecNum, uint8 level, sh2::InterruptSource source, uint32 pc) {}
 
-    // Invoked when the CPU handles an exception.
-    //
-    // `vecNum` is the exception vector number
-    // `pc` is the value of PC at the moment the exception was handled
-    // `sr` is the value of SR at the moment the exception was handled
+    /// @brief Invoked when the CPU handles an exception.
+    /// @param[in] vecNum the exception vector number
+    /// @param[in] pc the value of PC at the moment the exception was handled
+    /// @param[in] sr the value of SR at the moment the exception was handled
     virtual void Exception(uint8 vecNum, uint32 pc, uint32 sr) {}
 
-    // Invoked when a 32-bit by 32-bit division begins.
-    //
-    // `dividend` is the value of the dividend (DVDNTL)
-    // `divisor` is the value of the divisor (DVSR)
-    // `overflowIntrEnable` indicates if the division overflow interrupt is enabled (DVCR.OVFIE)
+    /// @brief Invoked when a 32-bit by 32-bit DIVU division begins.
+    /// @param[in] dividend the value of the dividend (`DVDNTL`)
+    /// @param[in] divisor the value of the divisor (`DVSR`)
+    /// @param[in] overflowIntrEnable indicates if the division overflow interrupt is enabled (`DVCR.OVFIE`)
     virtual void Begin32x32Division(sint32 dividend, sint32 divisor, bool overflowIntrEnable) {}
 
-    // Invoked when a 64-bit by 32-bit division begins.
-    //
-    // `dividend` is the value of the dividend (DVDNTH:DVDNTL)
-    // `divisor` is the value of the divisor (DVSR)
-    // `overflowIntrEnable` indicates if the division overflow interrupt is enabled (DVCR.OVFIE)
+    /// @brief Invoked when a 64-bit by 32-bit DIVU division begins.
+    /// @param[in] dividend the value of the dividend (`DVDNTH:DVDNTL`)
+    /// @param[in] divisor the value of the divisor (`DVSR`)
+    /// @param[in] overflowIntrEnable indicates if the division overflow interrupt is enabled (`DVCR.OVFIE`)
     virtual void Begin64x32Division(sint64 dividend, sint32 divisor, bool overflowIntrEnable) {}
 
-    // Invoked when a division ends.
-    //
-    // `quotient` is the resulting quotient (DVDNTL)
-    // `remainder` is the resulting remainder (DVDNTH)
-    // `overflow` indicates if the division resulted in an overflow
+    /// @brief Invoked when a DIVU division ends.
+    /// @param[in] quotient the resulting quotient (`DVDNTL`)
+    /// @param[in] remainder the resulting remainder (`DVDNTH`)
+    /// @param[in] overflow indicates if the division resulted in an overflow
     virtual void EndDivision(sint32 quotient, sint32 remainder, bool overflow) {}
 
-    // Invoked when a DMA transfer begins.
-    //
-    // `channel` is the DMAC channel number, either 0 or 1.
-    // `srcAddress` is the starting source address of the transfer.
-    // `dstAddress` is the starting destination address of the transfer.
-    // `count` is the number of transfer units to be performed. For 16-byte transfers, this number decrements once per
-    // 32-bit transfer.
-    // `unitSize` is the size of a single unit of transfer: 1, 2, 4 or 16.
-    // `srcInc` indicates the source address increment per unit of transfer.
-    // `dstInc` indicates the destination address increment per unit of transfer.
+    /// @brief Invoked when a DMA transfer begins.
+    /// @param[in] channel the DMAC channel number, either 0 or 1
+    /// @param[in] srcAddress the starting source address of the transfer
+    /// @param[in] dstAddress the starting destination address of the transfer
+    /// @param[in] count the number of transfer units to be performed. For 16-byte transfers, this number decrements
+    ///            once per 32-bit transfer
+    /// @param[in] unitSize the size of a single unit of transfer: 1, 2, 4 or 16
+    /// @param[in] srcInc the source address increment per unit of transfer
+    /// @param[in] dstInc the destination address increment per unit of transfer
     virtual void DMAXferBegin(uint32 channel, uint32 srcAddress, uint32 dstAddress, uint32 count, uint32 unitSize,
                               sint32 srcInc, sint32 dstInc) {}
 
-    // Invoked when a DMA channel transfers one unit of data.
-    // For 16-byte transfers, this function is invoked once per 32-bit transfer with a `unitSize` of 4.
-    //
-    // `channel` is the DMAC channel number, either 0 or 1.
-    // `srcAddress` is the source address of the transfer.
-    // `dstAddress` is the destination address of the transfer.
-    // `data` is the data transferred.
-    // `unitSize` is the size of a single unit of transfer: 1, 2 or 4.
+    /// @brief Invoked when a DMA channel transfers one unit of data.
+    ///
+    /// For 16-byte transfers, this function is invoked once per 32-bit transfer with a `unitSize` of 4.
+    ///
+    /// @param[in] channel the DMAC channel number, either 0 or 1
+    /// @param[in] srcAddress the source address of the transfer
+    /// @param[in] dstAddress the destination address of the transfer
+    /// @param[in] data the transferred data
+    /// @param[in] unitSize the size of a single unit of transfer: 1, 2 or 4
     virtual void DMAXferData(uint32 channel, uint32 srcAddress, uint32 dstAddress, uint32 data, uint32 unitSize) {}
 
-    // Invoked when a DMA transfer finishes.
-    //
-    // `channel` is the DMAC channel number, either 0 or 1.
-    // `irqRaised` indicates if the channel's transfer end interrupt signal was raised.
+    /// @brief Invoked when a DMA transfer finishes.
+    /// @param[in] channel the DMAC channel number, either 0 or 1
+    /// @param[in] irqRaised indicates if the channel's transfer end interrupt signal was raised
     virtual void DMAXferEnd(uint32 channel, bool irqRaised) {}
 };
 
