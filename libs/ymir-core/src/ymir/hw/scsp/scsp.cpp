86ed8d29942b8f988a124591c6f266e03ff73884
StrikerX3 <StrikerX3@users.noreply.github.com>
2025-05-02 22:23:28 -0300

    fix(SCSP): FM is only applied when MDL is at least 5

--- libs/ymir-core/src/ymir/hw/scsp/scsp.cpp
+++ libs/ymir-core/src/ymir/hw/scsp/scsp.cpp
@@ -688,7 +688,7 @@ FORCE_INLINE void SCSP::SlotProcessStep2(Slot &slot) {
     }
 
     sint32 modulation = 0;
-    if (slot.modLevel > 0 || slot.modXSelect != 0 || slot.modYSelect != 0) {
+    if (slot.modLevel >= 5) {
         const sint16 xd = m_soundStack[(m_soundStackIndex - 1 + slot.modXSelect) & 63];
         const sint16 yd = m_soundStack[(m_soundStackIndex - 1 + slot.modYSelect) & 63];
         const sint32 zd = (xd + yd) / 2;
