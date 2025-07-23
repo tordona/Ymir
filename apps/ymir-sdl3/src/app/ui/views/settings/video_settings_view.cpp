33833439718af789f86c812ae3e6e5d556c4c7c9
StrikerX3 <StrikerX3@users.noreply.github.com>
2025-07-22 23:45:29 -0300

    chore(settings): Clarify that the full refresh rate option uses an *integer* multiple of the target frame rate

--- apps/ymir-sdl3/src/app/ui/views/settings/video_settings_view.cpp
+++ apps/ymir-sdl3/src/app/ui/views/settings/video_settings_view.cpp
@@ -78,8 +78,8 @@ void VideoSettingsView::Display() {
 
     MakeDirty(ImGui::Checkbox("Use full refresh rate in full screen mode", &settings.useFullRefreshRateInFullScreen));
     widgets::ExplanationTooltip(
-        "When enabled, while in full screen, the GUI frame rate will be adjusted to the largest multiple of the "
-        "emulator's target frame rate that's not greater than your display's refresh rate.\n"
+        "When enabled, while in full screen, the GUI frame rate will be adjusted to the largest integer multiple of "
+        "the emulator's target frame rate that's not greater than your display's refresh rate.\n"
         "When disabled, the GUI frame rate will be limited to the emulator's target frame rate.\n"
         "Enabling this option can slightly reduce input latency on high refresh rate displays.",
         m_context.displayScale);
