4246bcc524847e33eaae2b9c62a94d320aa15435
Terence Ordona <terence.ordona@gmail.com>
2025-07-28 10:41:57 -0400

    fix (build): Use localtime_s on Windows ARM64
    This adds the _M_ARM64 check to address build failure when using MSYS2 environment on Windows ARM64

--- apps/ymir-sdl3/src/util/std_lib.cpp
+++ apps/ymir-sdl3/src/util/std_lib.cpp
@@ -5,7 +5,7 @@ namespace util {
 tm to_local_time(std::chrono::system_clock::time_point tp) {
     const time_t time = std::chrono::system_clock::to_time_t(tp);
     tm tm;
-#if defined(_MSC_VER)
+#if defined(_MSC_VER) || defined(_M_ARM64)
     void(localtime_s(&tm, &time));
 #elif defined(__GNUC__)
     localtime_r(&time, &tm);
