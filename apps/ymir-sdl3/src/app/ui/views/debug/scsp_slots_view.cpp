0c2a028c2deb7f525c1a9b0baf168c62a14ed371
StrikerX3 <StrikerX3@users.noreply.github.com>
2025-08-11 13:59:20 -0300

    feat(debug): Add several fields to SCSP slots view

--- apps/ymir-sdl3/src/app/ui/views/debug/scsp_slots_view.cpp
+++ apps/ymir-sdl3/src/app/ui/views/debug/scsp_slots_view.cpp
@@ -17,18 +17,35 @@ void SCSPSlotsView::Display() {
     ImGui::PushFont(m_context.fonts.monospace.regular, m_context.fontSizes.medium);
     const float hexCharWidth = ImGui::CalcTextSize("F").x;
     ImGui::PopFont();
+    const float msCharWidth = ImGui::CalcTextSize(ICON_MS_KEYBOARD_TAB).x;
 
     auto &probe = m_scsp.GetProbe();
     const auto &slots = probe.GetSlots();
 
     ImGui::BeginGroup();
 
-    if (ImGui::BeginTable("slots", 5, ImGuiTableFlags_SizingFixedFit)) {
-        ImGui::TableSetupColumn("#", ImGuiTableColumnFlags_WidthFixed, hexCharWidth * 2 + paddingWidth * 2);
-        ImGui::TableSetupColumn("SA", ImGuiTableColumnFlags_WidthFixed, hexCharWidth * 5 + paddingWidth * 2);
-        ImGui::TableSetupColumn("LSA", ImGuiTableColumnFlags_WidthFixed, hexCharWidth * 4 + paddingWidth * 2);
-        ImGui::TableSetupColumn("LEA", ImGuiTableColumnFlags_WidthFixed, hexCharWidth * 4 + paddingWidth * 2);
-        ImGui::TableSetupColumn("Loop", ImGuiTableColumnFlags_WidthFixed, hexCharWidth * 2 + paddingWidth * 2);
+    if (ImGui::BeginTable("slots", 15, ImGuiTableFlags_SizingFixedFit)) {
+        ImGui::TableSetupColumn("#", ImGuiTableColumnFlags_WidthFixed, hexCharWidth * 2);
+        ImGui::TableSetupColumn("KYONB", ImGuiTableColumnFlags_WidthFixed, msCharWidth);
+        ImGui::TableSetupColumn("SA", ImGuiTableColumnFlags_WidthFixed, hexCharWidth * 5);
+        ImGui::TableSetupColumn("LSA", ImGuiTableColumnFlags_WidthFixed, hexCharWidth * 4);
+        ImGui::TableSetupColumn("LEA", ImGuiTableColumnFlags_WidthFixed, hexCharWidth * 4);
+        ImGui::TableSetupColumn("Loop", ImGuiTableColumnFlags_WidthFixed, msCharWidth);
+        ImGui::TableSetupColumn("Bits", ImGuiTableColumnFlags_WidthFixed, hexCharWidth * 2);
+        ImGui::TableSetupColumn("SBCTL", ImGuiTableColumnFlags_WidthFixed, hexCharWidth * 4);
+        ImGui::TableSetupColumn("Source", ImGuiTableColumnFlags_WidthFixed, hexCharWidth * 4 + paddingWidth);
+        ImGui::TableSetupColumn("AR", ImGuiTableColumnFlags_WidthFixed, hexCharWidth * 2);
+        ImGui::TableSetupColumn("D1R", ImGuiTableColumnFlags_WidthFixed, hexCharWidth * 2);
+        ImGui::TableSetupColumn("D2R", ImGuiTableColumnFlags_WidthFixed, hexCharWidth * 2);
+        ImGui::TableSetupColumn("RR", ImGuiTableColumnFlags_WidthFixed, hexCharWidth * 2);
+        ImGui::TableSetupColumn("DL", ImGuiTableColumnFlags_WidthFixed, hexCharWidth * 2);
+        ImGui::TableSetupColumn("KRS", ImGuiTableColumnFlags_WidthFixed, hexCharWidth * 1);
+        // TODO: EGHOLD
+        // TODO: LPSLNK
+        // TODO: EGBYPASS + padding
+        // TODO: MDL
+        // TODO: MDXSL
+        // TODO: MDYSL
         ImGui::TableSetupScrollFreeze(1, 1);
         ImGui::TableHeadersRow();
 
@@ -41,11 +58,17 @@ void SCSPSlotsView::Display() {
             if (disabled) {
                 ImGui::BeginDisabled();
             }
+
             ImGui::TableNextRow();
             if (ImGui::TableNextColumn()) {
                 ImGui::PushFont(m_context.fonts.monospace.regular, m_context.fontSizes.medium);
                 ImGui::Text("%02d", i);
                 ImGui::PopFont();
+            }
+            if (ImGui::TableNextColumn()) {
+                if (slot.keyOnBit) {
+                    ImGui::TextUnformatted(ICON_MS_PLAY_ARROW);
+                }
             }
             if (ImGui::TableNextColumn()) {
                 ImGui::PushFont(m_context.fonts.monospace.regular, m_context.fontSizes.medium);
@@ -83,6 +106,75 @@ void SCSPSlotsView::Display() {
                     break;
                 }
             }
+            if (ImGui::TableNextColumn()) {
+                ImGui::PushFont(m_context.fonts.monospace.regular, m_context.fontSizes.medium);
+                ImGui::TextUnformatted(slot.pcm8Bit ? " 8" : "16");
+                ImGui::PopFont();
+            }
+            if (ImGui::TableNextColumn()) {
+                ImGui::PushFont(m_context.fonts.monospace.regular, m_context.fontSizes.medium);
+                ImGui::Text("%04X", slot.sampleXOR);
+                ImGui::PopFont();
+            }
+            if (ImGui::TableNextColumn()) {
+                using enum scsp::Slot::SoundSource;
+                const char *soundSourceText = "????";
+                const char *soundSourceHint = "Unknown";
+                switch (slot.soundSource) {
+                case SoundRAM:
+                    soundSourceText = "SRAM";
+                    soundSourceHint = "Sound RAM";
+                    break;
+                case Noise:
+                    soundSourceText = "LFSR";
+                    soundSourceHint = "Noise";
+                    break;
+                case Silence:
+                    soundSourceText = "ZERO";
+                    soundSourceHint = "Silence";
+                    break;
+                case Unknown:
+                    soundSourceText = "????";
+                    soundSourceHint = "Unknown";
+                    break;
+                }
+
+                ImGui::PushFont(m_context.fonts.monospace.regular, m_context.fontSizes.medium);
+                ImGui::TextUnformatted(soundSourceText);
+                ImGui::PopFont();
+                ImGui::SetItemTooltip("%s", soundSourceHint);
+            }
+            if (ImGui::TableNextColumn()) {
+                ImGui::PushFont(m_context.fonts.monospace.regular, m_context.fontSizes.medium);
+                ImGui::Text("%02X", slot.attackRate);
+                ImGui::PopFont();
+            }
+            if (ImGui::TableNextColumn()) {
+                ImGui::PushFont(m_context.fonts.monospace.regular, m_context.fontSizes.medium);
+                ImGui::Text("%02X", slot.decay1Rate);
+                ImGui::PopFont();
+            }
+            if (ImGui::TableNextColumn()) {
+                ImGui::PushFont(m_context.fonts.monospace.regular, m_context.fontSizes.medium);
+                ImGui::Text("%02X", slot.decay2Rate);
+                ImGui::PopFont();
+            }
+            if (ImGui::TableNextColumn()) {
+                ImGui::PushFont(m_context.fonts.monospace.regular, m_context.fontSizes.medium);
+                ImGui::Text("%02X", slot.releaseRate);
+                ImGui::PopFont();
+            }
+            if (ImGui::TableNextColumn()) {
+                ImGui::PushFont(m_context.fonts.monospace.regular, m_context.fontSizes.medium);
+                ImGui::Text("%02X", slot.decayLevel);
+                ImGui::PopFont();
+            }
+            if (ImGui::TableNextColumn()) {
+                ImGui::PushFont(m_context.fonts.monospace.regular, m_context.fontSizes.medium);
+                ImGui::Text("%X", slot.keyRateScaling);
+                ImGui::PopFont();
+            }
+
             if (disabled) {
                 ImGui::EndDisabled();
             }
