ce0dda925d1a45c9089e6ec9c3e738edb9da07dc
StrikerX3 <StrikerX3@users.noreply.github.com>
2025-08-14 13:25:21 -0300

    feat(debug): Allow exporting debug output to a file

--- apps/ymir-sdl3/src/app/ui/views/debug/debug_output_view.cpp
+++ apps/ymir-sdl3/src/app/ui/views/debug/debug_output_view.cpp
@@ -1,5 +1,9 @@
 #include "debug_output_view.hpp"
 
+#include <app/events/gui_event_factory.hpp>
+
+#include <util/sdl_file_dialog.hpp>
+
 namespace app::ui {
 
 DebugOutputView::DebugOutputView(SharedContext &context)
@@ -10,17 +14,56 @@ void DebugOutputView::Display() {
     if (ImGui::Button("Clear##debug_output")) {
         m_tracer.ClearDebugMessages();
     }
+    ImGui::SameLine();
+    if (ImGui::Button("Save to file...##debug_output")) {
+        m_context.EnqueueEvent(events::gui::SaveFile(
+            {.dialogTitle = "Export debug output",
+             .defaultPath = m_context.profile.GetPath(ProfilePath::Dumps) / "debug.txt",
+             .filters = {{.name = "Text files (*.txt)", .filters = "txt"}},
+             .userdata = this,
+             .callback = util::WrapSingleSelectionCallback<&DebugOutputView::ProcessExportDebugOutput,
+                                                           util::NoopCancelFileDialogCallback,
+                                                           &DebugOutputView::ProcessExportError>}));
+    }
 
     if (ImGui::BeginChild("##scu_debug_output")) {
         ImGui::PushFont(m_context.fonts.monospace.regular, m_context.fontSizes.small);
+        std::string fullBuffer{};
         const size_t count = m_tracer.debugMessages.Count();
         for (size_t i = 0; i < count; i++) {
-            ImGui::Text("%s", m_tracer.debugMessages.Read(i).c_str());
+            fullBuffer.append(m_tracer.debugMessages.Read(i)).append("\n");
         }
-        ImGui::Text("%s", m_tracer.GetDebugMessageBuffer().data());
+        fullBuffer.append(m_tracer.GetDebugMessageBuffer());
+        ImGui::InputTextMultiline("##debug_output", fullBuffer.data(), fullBuffer.size(),
+                                  ImGui::GetContentRegionAvail(), ImGuiInputTextFlags_ReadOnly);
         ImGui::PopFont();
     }
     ImGui::EndChild();
+}
+
+void DebugOutputView::ProcessExportDebugOutput(void *userdata, std::filesystem::path file, int filter) {
+    static_cast<DebugOutputView *>(userdata)->ExportDebugOutput(file);
+}
+
+void DebugOutputView::ProcessCancelExport(void *userdata, int filter) {}
+
+void DebugOutputView::ProcessExportError(void *userdata, const char *errorMessage, int filter) {
+    static_cast<DebugOutputView *>(userdata)->ShowErrorDialog(errorMessage);
+}
+
+void DebugOutputView::ExportDebugOutput(std::filesystem::path file) {
+    std::filesystem::create_directories(file.parent_path());
+
+    std::ofstream out{file};
+    const size_t count = m_tracer.debugMessages.Count();
+    for (size_t i = 0; i < count; i++) {
+        out << m_tracer.debugMessages.Read(i) << "\n";
+    }
+    out << m_tracer.GetDebugMessageBuffer();
+}
+
+void DebugOutputView::ShowErrorDialog(const char *message) {
+    m_context.EnqueueEvent(events::gui::ShowError(message));
 }
 
 } // namespace app::ui
--- apps/ymir-sdl3/src/app/ui/views/debug/debug_output_view.hpp
+++ apps/ymir-sdl3/src/app/ui/views/debug/debug_output_view.hpp
@@ -15,6 +15,13 @@ public:
 private:
     SharedContext &m_context;
     SCUTracer &m_tracer;
+
+    static void ProcessExportDebugOutput(void *userdata, std::filesystem::path file, int filter);
+    static void ProcessCancelExport(void *userdata, int filter);
+    static void ProcessExportError(void *userdata, const char *errorMessage, int filter);
+
+    void ExportDebugOutput(std::filesystem::path file);
+    void ShowErrorDialog(const char *message);
 };
 
 } // namespace app::ui
