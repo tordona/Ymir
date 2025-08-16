e80e50bcab04efa7a000322ccf464cfdcc57e8af
StrikerX3 <StrikerX3@users.noreply.github.com>
2025-08-16 13:49:43 -0300

    feat(sandbox): VDP1 accuracy tests

--- apps/ymir-sandbox/src/main.cpp
+++ apps/ymir-sandbox/src/main.cpp
@@ -4,11 +4,20 @@
 
 #include <ymir/sys/backup_ram.hpp>
 
+#include <ymir/hw/vdp/vdp.hpp>
+
 #include <ymir/util/size_ops.hpp>
 
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_events.h>
 #include <fmt/format.h>
+#include <fmt/std.h>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include <stb_image_write.h>
 
 #include <chrono>
 #include <filesystem>
@@ -921,10 +930,152 @@ void runInputSandbox() {
     SDL_DestroyRenderer(renderer);
 }
 
+struct sample_struct {
+    const char *vramFile;
+    const char *cramFile;
+    const char *fbFile;
+    int width;
+    int height;
+};
+
+// clang-format off
+const sample_struct g_samples[] = {
+   // VRAM                      Color-RAM             HW-framebuffer as bmp     W    H 
+    { "srally3.bin",            "srally3_cram.bin",   "srally3.bmp",            352, 224 },
+    { "gouraud_lines.bin",      "lzsscube_cram.bin",  "gouraud_lines.bmp",      320, 224 },
+    { "twisted2.bin",           "lines_cram.bin",     "twisted2.bmp",           352, 224 },
+    { "sprites2.bin",           "lines_cram.bin",     "sprites2.bmp",           352, 224 },
+    { "sprites_anti.bin",       "lines_cram.bin",     "sprites_anti.bmp",       352, 224 },
+    { "sprites_anti_r.bin",     "lines_cram.bin",     "sprites_anti_r.bmp",     352, 224 },
+    { "sprites_horizontal.bin", "lines_cram.bin",     "sprites_horizontal.bmp", 352, 224 },
+    { "twisted_horizontal.bin", "lines_cram.bin",     "twisted_horizontal.bmp", 352, 224 },
+    { "twisted_box2.bin",       "lines_cram.bin",     "twisted_box2.bmp",       352, 224 },
+    { "twisted_box3.bin",       "lines_cram.bin",     "twisted_box3.bmp",       352, 224 },
+    { "pixel_scale.bin",        "lines_cram.bin",     "pixel_scale.bmp",        352, 224 },
+    { "gouraud_short.bin",      "lzsscube_cram.bin",  "gouraud_short.bmp",      320, 224 },
+    { "gouraud_test.bin",       "lzsscube_cram.bin",  "gouraud_test.bmp",       320, 224 },
+    { "gouraud_test2.bin",      "lzsscube_cram.bin",  "gouraud_test2.bmp",      320, 224 },
+    { "ninpen_rangers.bin",     "lzsscube_cram.bin",  "ninpen_rangers.bmp",     320, 224 }
+};
+// clang-format on
+
+void runVDP1AccuracySandbox(std::filesystem::path testPath) {
+    fmt::println("Reading tests from {}", testPath);
+
+    for (auto &test : g_samples) {
+        fmt::println("VRAM: {}", test.vramFile);
+        fmt::println("CRAM: {}", test.cramFile);
+        fmt::println("FB:   {}", test.fbFile);
+        fmt::println("Size: {}x{}", test.width, test.height);
+
+        bool renderDone = false;
+        ymir::core::Scheduler scheduler{};
+        ymir::core::Configuration config{};
+        config.video.threadedVDP = false;
+        config.system.videoStandard = ymir::core::config::sys::VideoStandard::NTSC;
+        auto vdp = std::make_unique<ymir::vdp::VDP>(scheduler, config);
+        vdp->SetVDP1DrawCallback({&renderDone, [](void *ctx) { *static_cast<bool *>(ctx) = true; }});
+
+        auto vramPath = testPath / test.vramFile;
+        auto cramPath = testPath / test.cramFile;
+        auto fbPath = testPath / test.fbFile;
+
+        std::vector<uint8> cram{};
+
+        {
+            std::ifstream in{vramPath, std::ios::binary};
+            if (!in) {
+                fmt::println("WARNING: file {} not found", vramPath);
+            }
+            for (uint32 addr = 0; addr < ymir::vdp::kVDP1VRAMSize; ++addr) {
+                const uint8 value = in.get();
+                vdp->VDP1WriteVRAM<uint8, true>(addr, value);
+            }
+        }
+        {
+            std::ifstream in{cramPath, std::ios::binary};
+            if (!in) {
+                fmt::println("WARNING: file {} not found", vramPath);
+            }
+            cram.resize(ymir::vdp::kVDP2CRAMSize);
+            in.read((char *)cram.data(), ymir::vdp::kVDP2CRAMSize);
+        }
+
+        vdp->VDP1WriteReg<false>(0x04, 3); // PTMR
+        vdp->VDP1WriteReg<false>(0x06, 0); // EWDR
+
+        while (!renderDone) {
+            const uint64 cycles = scheduler.NextCount();
+            vdp->Advance<false>(cycles);
+            scheduler.Advance(cycles);
+        }
+
+        auto vdp1fb = vdp->VDP1GetDrawFramebuffer();
+        std::vector<uint32> finalFB{};
+        finalFB.resize(test.width * test.height);
+        for (uint32 y = 0; y < test.height; ++y) {
+            for (uint32 x = 0; x < test.width; ++x) {
+                const uint32 fbOffset = (y * 512 + x) * sizeof(uint16);
+                const uint16 spriteData = util::ReadBE<uint16>(&vdp1fb[fbOffset & 0x3FFFF]);
+
+                // Assuming mixed mode and ignoring shadows for now
+                uint32 r, g, b;
+                if (bit::test<15>(spriteData)) {
+                    // RGB data
+                    r = bit::extract<0, 4>(spriteData) << 3u;
+                    g = bit::extract<5, 9>(spriteData) << 3u;
+                    b = bit::extract<10, 14>(spriteData) << 3u;
+                } else if (spriteData == 0) {
+                    // Transparent
+                    r = 0xFF;
+                    g = 0x00;
+                    b = 0xFF;
+                } else {
+                    // Palette data
+                    const uint32 colorData = util::ReadBE<uint16>(&cram[(spriteData << 1u) & 0xFFE]);
+                    r = bit::extract<0, 4>(spriteData) << 3u;
+                    g = bit::extract<5, 9>(spriteData) << 3u;
+                    b = bit::extract<10, 14>(spriteData) << 3u;
+                }
+                finalFB[y * test.width + x] = (0xFF << 24u) | (b << 16u) | (g << 8u) | (r << 0u);
+            }
+        }
+
+        auto outPath = testPath / "out";
+        auto filename = std::filesystem::path(test.fbFile).replace_extension("").string();
+        auto outFile = outPath / fmt::format("{}-final.png", filename);
+        std::filesystem::create_directories(outPath);
+        stbi_write_png(outFile.string().c_str(), test.width, test.height, 4, finalFB.data(),
+                       test.width * sizeof(uint32));
+
+        int imgX, imgY, ch;
+        stbi_uc *img = stbi_load(fbPath.string().c_str(), &imgX, &imgY, &ch, 4);
+        std::vector<uint32> deltaFB = finalFB;
+        if (img != nullptr) {
+            for (uint32 i = 0; i < deltaFB.size(); ++i) {
+                deltaFB[i] ^= reinterpret_cast<uint32 *>(img)[i];
+                if (deltaFB[i] & 0xFFFFFF) {
+                    deltaFB[i] |= 0xFF000000;
+                }
+            }
+            auto deltaFile = outPath / fmt::format("{}-delta.png", filename);
+            stbi_write_png(deltaFile.string().c_str(), test.width, test.height, 4, deltaFB.data(),
+                           test.width * sizeof(uint32));
+        } else {
+            fmt::println("WARNING: file {} not found", fbPath);
+        }
+
+        stbi_image_free(img);
+    }
+}
+
 int main(int argc, char **argv) {
     // runSandbox();
     // runBUPSandbox();
-    runInputSandbox();
+    // runInputSandbox();
+    if (argc >= 2) {
+        runVDP1AccuracySandbox(argv[1]);
+    }
 
     return EXIT_SUCCESS;
 }
